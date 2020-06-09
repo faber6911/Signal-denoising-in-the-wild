e64fa3d5e600bacc8aa9bb61d2bc1d9385364c3c
root <root@gpu-sm-02.fsoc.hpi.uni-potsdam.de>
2020-06-09 21:09:23 +0000

    updates

--- pyscripts/utils.py
+++ pyscripts/utils.py
@@ -1,8 +1,9 @@
+import os
+
 import numpy as np
 import torch
 import librosa
 import kaldi_io
-
 
 def rms(x):
     return np.sqrt(np.mean(np.square(x)))
@@ -85,3 +86,52 @@ class DenoiseDataset(torch.utils.data.Dataset):
         y = y[pin:pin+self.seq_len]
         
         return chunk_mat, y
+
+    
+class AudioDataset(torch.utils.data.Dataset):
+    """
+    Audio sample reader.
+    """
+
+    def __init__(self, data_type):
+
+        if data_type == 'train':
+            data_path = '../data/serialized_train_data/'
+        else:
+            data_path = '../data/serialized_test_data/'
+        if not os.path.exists(data_path):
+            raise FileNotFoundError('The {} data folder does not exist!'.format(data_type))
+
+        self.data_type = data_type
+        self.file_names = [os.path.join(data_path, filename) for filename in os.listdir(data_path)]
+
+#     def reference_batch(self, batch_size):
+#         """
+#         Randomly selects a reference batch from dataset.
+#         Reference batch is used for calculating statistics for virtual batch normalization operation.
+#         Args:
+#             batch_size(int): batch size
+#         Returns:
+#             ref_batch: reference batch
+#         """
+#         ref_file_names = np.random.choice(self.file_names, batch_size)
+#         ref_batch = np.stack([np.load(f) for f in ref_file_names])
+
+#         ref_batch = emphasis(ref_batch, emph_coeff=0.95)
+#         return torch.from_numpy(ref_batch).type(torch.FloatTensor)
+
+    def __getitem__(self, idx):
+        pair = np.load(self.file_names[idx])
+        #pair = emphasis(pair[np.newaxis, :, :], emph_coeff=0.95).reshape(2, -1)
+        clean = pair[0].reshape(1, -1)
+        noisy = pair[1].reshape(1, -1)
+        return(torch.from_numpy(clean).type(torch.FloatTensor), torch.from_numpy(noisy).type(torch.FloatTensor))
+#         if self.data_type == 'train':
+#             clean = pair[0].reshape(1, -1)
+#             return torch.from_numpy(pair).type(torch.FloatTensor), torch.from_numpy(clean).type(
+#                 torch.FloatTensor), torch.from_numpy(noisy).type(torch.FloatTensor)
+#         else:
+#             return os.path.basename(self.file_names[idx]), torch.from_numpy(noisy).type(torch.FloatTensor)
+
+    def __len__(self):
+        return len(self.file_names)
